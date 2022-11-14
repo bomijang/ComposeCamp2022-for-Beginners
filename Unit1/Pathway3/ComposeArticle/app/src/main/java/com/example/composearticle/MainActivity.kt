7e819bcc4fefe6ac1979a7deb7fd1011938c6f75
bomi.jang <bomi.jang@lgensol.com>
2022-11-14 17:32:41 +0900

    [Week1][연습] Compose Article

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -24,12 +26,27 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent { 
+            androidx.compose.material.Surface {
+                ComposeArticleApp()
+            }
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() { 
+    val imagePainter = painterResource(id = R.drawable.bg_compose_background)
+    var title = stringResource(R.string.title_text)
+    var shortDescription = stringResource(R.string.shor_desc)
+    var longDescription = stringResource(R.string.long_desc)
+    ArticleCard(
+        title = title,
+        shortDescription = shortDescription,
+        longDescription = longDescription,
+        imagePainter = imagePainter
+    )
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +56,32 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column {
+        Image(painter = imagePainter, 
+            contentDescription = null,
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start=16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(text = longDescription,
+            modifier = Modifier.padding(16.dp), 
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeArticleTheme{
+        ComposeArticleApp()
+    }
+}
